8c8aceb44abbdd6073fc24d1e856f8f35f8ba48d
Peter Ivanov <ivanovp@gmail.com>
2019-10-20 19:40:36 +0200

    WiFi, USB audio enabled and MP3 players were added.

---
+++ board/olimex/imx233_olinuxino/post_build.sh
@@ -0,0 +1,3 @@
+cp board/olimex/imx233_olinuxino/interfaces ${TARGET_DIR}/etc/network/interfaces
+cp board/olimex/imx233_olinuxino/wpa_supplicant.conf ${TARGET_DIR}/etc/wpa_supplicant.conf
+cp -rv board/olimex/imx233_olinuxino/bt-audio /root
