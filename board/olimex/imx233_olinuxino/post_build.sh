bb58148e802212f2b23b36768bb006812b1cf532
Peter Ivanov <ivanovp@gmail.com>
2019-10-22 19:02:19 +0200

    Build scripts are used. Filesystem is ext4.

--- board/olimex/imx233_olinuxino/post_build.sh
+++ board/olimex/imx233_olinuxino/post_build.sh
@@ -1,3 +1,5 @@
-cp board/olimex/imx233_olinuxino/interfaces ${TARGET_DIR}/etc/network/interfaces
-cp board/olimex/imx233_olinuxino/wpa_supplicant.conf ${TARGET_DIR}/etc/wpa_supplicant.conf
-cp -rv board/olimex/imx233_olinuxino/bt-audio /root
+#!/bin/sh
+echo "--- iMX233-OLinuXino ---"
+cp -v board/olimex/imx233_olinuxino/interfaces ${TARGET_DIR}/etc/network/interfaces
+cp -v board/olimex/imx233_olinuxino/wpa_supplicant.conf ${TARGET_DIR}/etc/wpa_supplicant.conf
+cp -rv board/olimex/imx233_olinuxino/bt-audio ${TARGET_DIR}/root
