0c6d1e5a63118370c6155fa6d0487cad1c339308
Peter Ivanov <ivanovp@gmail.com>
2019-10-22 22:05:29 +0200

    Config files were moved. vsftpd and minicom were configured.

--- board/olimex/imx233_olinuxino/post_build.sh
+++ board/olimex/imx233_olinuxino/post_build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 echo "--- iMX233-OLinuXino ---"
-cp -v board/olimex/imx233_olinuxino/interfaces ${TARGET_DIR}/etc/network/interfaces
-cp -v board/olimex/imx233_olinuxino/wpa_supplicant.conf ${TARGET_DIR}/etc/wpa_supplicant.conf
+cp -rv board/olimex/imx233_olinuxino/etc ${TARGET_DIR}
 cp -rv board/olimex/imx233_olinuxino/bt-audio ${TARGET_DIR}/root
