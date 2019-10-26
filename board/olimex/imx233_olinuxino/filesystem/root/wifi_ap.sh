9064f6bbd16262e7a7ca0a6e6631b846974bfc0b
Peter Ivanov <ivanovp@gmail.com>
2019-10-26 17:29:58 +0200

    Size of root FS was increased.

--- board/olimex/imx233_olinuxino/filesystem/root/wifi_ap.sh
+++ board/olimex/imx233_olinuxino/filesystem/root/wifi_ap.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 rm /dev/random
 ln -s /dev/urandom /dev/random
+ifdown wlan0
+killall -9 wpa_supplicant
+ifconfig wlan0 192.168.10.1
 # -B to run in background
-wpa_supplicant -Dnl80211 -iwlan0 -c/etc/wpa_supplicant_ap.conf
+wpa_supplicant -B -Dnl80211 -iwlan0 -c/etc/wpa_supplicant_ap.conf
+udhcpd /etc/udhcpd.conf
 
