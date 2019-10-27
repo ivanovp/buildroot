64d5c7ba2eb533536fc056a79fcc762b069ade4a
Peter Ivanov <ivanovp@gmail.com>
2019-10-27 09:49:29 +0100

    python-mpd2 was added.

---
+++ package/python-mpd2/python-mpd2.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-mpd2
+#
+################################################################################
+
+PYTHON_MPD2_VERSION = 1.0.0
+PYTHON_MPD2_SOURCE = python-mpd2-$(PYTHON_MPD2_VERSION).tar.bz2
+PYTHON_MPD2_SITE = https://files.pythonhosted.org/packages/a8/12/63bdb3ee8e0bd7dd0476e79f0f130c1caeff408a1b1e5531ae9891805f7d
+PYTHON_MPD2_LICENSE = LGPLv3
+PYTHON_MPD2_LICENSE_FILES = LICENSE.txt
+PYTHON_MPD2_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
