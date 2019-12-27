6197a28e50a3f538f68637a8638f7cb961813607
Peter Ivanov <ivanovp@gmail.com>
2019-12-27 14:25:14 +0100

    Pure-ftpd is compiled with ANON_CAN_CHANGE_PERMS, ANON_CAN_DELETE, ANON_CAN_RESUME, ANON_CAN_RENAME. FTP users and groups are added automatically.

--- board/olimex/imx233_olinuxino/post_build.sh
+++ board/olimex/imx233_olinuxino/post_build.sh
@@ -4,4 +4,19 @@ rm -f ${TARGET_DIR}/etc/sudoers
 cp -rv board/olimex/imx233_olinuxino/filesystem/* ${TARGET_DIR}
 chmod 440 ${TARGET_DIR}/etc/sudoers
 chmod o+w ${TARGET_DIR}/etc/wpa_supplicant.conf
-
+#chgrp ftp ${TARGET_DIR}/var/lib/mpd/music
+#chmod g+w ${TARGET_DIR}/var/lib/mpd/music
+FTPUSER=`grep ftp ${TARGET_DIR}/etc/passwd`
+if [ "$FTPUSER" == "" ]; then
+    echo "--------------------> adding user"
+    echo "ftp:x:900:900:Anonymous FTP User,,,:/var/lib/mpd/music:/bin/false" >>${TARGET_DIR}/etc/passwd
+else
+    echo "--------------------> user already added"
+fi
+FTPGRP=`grep ftp ${TARGET_DIR}/etc/group`
+if [ "$FTPGRP" == "" ]; then
+    echo "--------------------> adding group"
+    echo "ftp:x:900:ftp" >>${TARGET_DIR}/etc/group
+else
+    echo "--------------------> group already added"
+fi
