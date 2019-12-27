409f0a542d53ddcb44b88ec1e3c6843f8c18337d
Peter Ivanov <ivanovp@gmail.com>
2019-12-27 16:09:29 +0100

    Configuration of Pure-FTPD was changed. post_build.sh was tuned.

--- board/olimex/imx233_olinuxino/post_build.sh
+++ board/olimex/imx233_olinuxino/post_build.sh
@@ -8,15 +8,15 @@ chmod o+w ${TARGET_DIR}/etc/wpa_supplicant.conf
 #chmod g+w ${TARGET_DIR}/var/lib/mpd/music
 FTPUSER=`grep ftp ${TARGET_DIR}/etc/passwd`
 if [ "$FTPUSER" == "" ]; then
-    echo "--------------------> adding user"
+    echo "---> adding ftp user"
     echo "ftp:x:900:900:Anonymous FTP User,,,:/var/lib/mpd/music:/bin/false" >>${TARGET_DIR}/etc/passwd
 else
-    echo "--------------------> user already added"
+    echo "---> ftp user already added"
 fi
 FTPGRP=`grep ftp ${TARGET_DIR}/etc/group`
 if [ "$FTPGRP" == "" ]; then
-    echo "--------------------> adding group"
+    echo "---> adding ftp group"
     echo "ftp:x:900:ftp" >>${TARGET_DIR}/etc/group
 else
-    echo "--------------------> group already added"
+    echo "---> ftp group already added"
 fi
